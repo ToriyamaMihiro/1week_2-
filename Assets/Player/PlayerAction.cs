bb90d0e2299276e767a7b1e4319fe8d954c59493
ToriyamaMihiro <k022g0003@g.neec.ac.jp>
2023-05-31 02:14:20 +0900

    移動時パーティクルの追加

--- Assets/Player/PlayerAction.cs
+++ Assets/Player/PlayerAction.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class PlayerAction : MonoBehaviour
 {
     //public GameObject box;
-    public BoxCollider2D col;
+    public GameObject particlePrefab;
 
     Rigidbody2D rigidbody2D;
 
@@ -25,6 +25,8 @@ public class PlayerAction : MonoBehaviour
     bool isLeft = true;
     bool isHit = false;
     bool isJump = false;
+    int particleTime = 0;
+    int particleCoolTime = 60;
     private Rigidbody2D rb = null;
 
     // Start is called before the first frame update
@@ -37,22 +39,35 @@ public class PlayerAction : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        particleTime++;
         /*---- ÉLÅ[à⁄ìÆ ----*/
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
         {
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 0);
+            if (particleTime % particleCoolTime == 0)
+            {
+                Instantiate(particlePrefab, transform.position, Quaternion.identity);
+                Instantiate(particlePrefab, transform.position, Quaternion.identity);
+            }
             isRight = false;
             isLeft = true;
         }
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
             this.GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
+            if (particleTime % particleCoolTime == 0)
+            {
+                Instantiate(particlePrefab, transform.position, Quaternion.identity);
+                Instantiate(particlePrefab, transform.position, Quaternion.identity);
+            }
             isRight = true;
             isLeft = false;
         }
         if (Input.GetKeyDown(KeyCode.Space) && isJump)
         {
             this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpPower), ForceMode2D.Impulse);
+
+
 
         }
         /*---- à⁄ìÆêßå¿ ----*/
